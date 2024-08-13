705cf13e47ff25dd3f00da480566e0e44c553287
FelixFras <f.frasch@gmx.net>
2024-08-13 15:20:59 +0200

    added buttons to show the history

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-const quoteElement = document.getElementById('quote');
-const authorElement = document.getElementById('author');
+const arrayLimit = 20;
+let quotes = [];
+let authors = [];
+let currentQuote = 0;
 
 function newQuote() {
+    let quote = "";
+    let author = "";
+
     fetch('https://api.quotable.io/random')
         .then(response => response.json())
         .then(data => {
-            const quote = data.content;
-            const author = data.author;
-            quoteElement.innerText = '"' + quote + '"';
-            authorElement.innerText = "~ " + author;
+            quotes.push(data.content);
+            authors.push(data.author);
+
+            if (quotes.length > arrayLimit) {
+                quotes.shift();
+                authors.shift();
+            }
+
+            showQuote(quotes.length - 1);
+            currentQuote = quotes.length - 1;
         })
         .catch(error => console.error('Error:', error));
+}
+
+function showQuote(i) {
+    const quoteElement = document.getElementById('quote');
+    const authorElement = document.getElementById('author');
+
+    quoteElement.innerText = '"' + quotes[i] + '"';
+    authorElement.innerText = "~ " + authors[i];
+}
+
+function showPrevious() {
+    if (currentQuote > 0) {
+        currentQuote--;
+    }
+    showQuote(currentQuote);
+}
+
+function showNext() {
+    if (currentQuote < quotes.length - 1) {
+        currentQuote++;
+    }
+    showQuote(currentQuote);
 }
 
 newQuote();
